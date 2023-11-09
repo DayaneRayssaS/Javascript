f6392143b24264255db1a01b3ac77c4e3769f7e9
DayaneRayssaS <128699223+DayaneRayssaS@users.noreply.github.com>
2023-11-09 08:50:41 -0300

    Add files via upload

---
+++ exercicio9.js
@@ -0,0 +1,24 @@
+const mensagensDivertidas =
+{
+    
+    "Qual é o desastre natural que os cães mais odeiam?  O Fura-cão"
+"O engenheiro olhou para o espelho. O que aconteceu?  O engenheiro civil;"
+"O engenheiro olhou para o espelho. O que aconteceu?  O engenheiro civil"
+"Tenho uma enxada, uma pá e uma foice. Quantas ferramentas eu tenho?   Duas, porque uma foi-se"
+"Por que o jacaré tirou o jacarezinho da escola?  Porque ele réptil de ano"
+"O que o azeite disse para o vinagre?  Falo nada, só óleo"
+
+}
+
+let botãoDivertido = document.getElementById ("botãoDivertido")
+let mensagemDivertido = document.getElementById("click, function")
+
+botãoDivertido.addEventListener('click', function() {
+
+    
+
+    const mensagemAleatoria = mensagensDivertidas {Math.floor(math.randon() = mensagensDivertidas.length)}
+    
+    mensagemDivertida.textContend = mensagemAleatoria;
+    
+} )
